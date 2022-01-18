9d70b81306a25e5eda962cfefd936395457b01a5
Debas Gebreslasie <65129228+Debas-31@users.noreply.github.com>
2022-01-18 22:39:40 +0300

    Implement re-usable components and bookdata as prop

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import BooksPage from '../redux/books/books';
-import CategoriesPage from '../redux/categories/categories';
+import Books from './Books';
+import Categories from './Categories';
 import Header from './Header';
 
 const App = () => (
-  <>
+  <div className="top-container">
     <Header />
     <Routes>
-      <Route exact path="/" element={<BooksPage />} />
-      <Route exact path="/categories" element={<CategoriesPage />} />
+      <Route exact path="/" element={<Books />} />
+      <Route exact path="/categories" element={<Categories />} />
     </Routes>
-  </>
+  </div>
 );
 
 export default App;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,36 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
-const Header = () => (
-  <>
-    <nav className="navigation-bar d-flex">
-      <h1>Bookstore</h1>
-      <ul className="d-flex">
-        <li>
-          <Link to="/">BOOKS</Link>
-        </li>
-        <li>
-          <Link to="/categories">CATEGORIES</Link>
-        </li>
-      </ul>
-    </nav>
-  </>
-);
+const Header = () => {
+  const links = [
+    {
+      id: 0,
+      path: '/',
+      text: 'BOOKS',
+    },
+    {
+      id: 1,
+      path: '/categories',
+      text: 'CATEGORIES',
+    },
+  ];
+
+  return (
+    <header className="header">
+      <h1 className="header-h1">Bookstore CMS</h1>
+      <nav>
+        <ul className="nav-links">
+          {links.map((link) => (
+            <li key={link.id}>
+              <NavLink to={link.path} className="nav-item">
+                {link.text}
+              </NavLink>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </header>
+  );
+};
 
 export default Header;
