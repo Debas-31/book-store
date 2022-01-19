db6fb3adc7aa8003fa49534219ffbc64ce4cd37a
Debas Gebreslasie <65129228+Debas-31@users.noreply.github.com>
2022-01-19 08:00:44 +0300

    Modify and arrange Events

--- src/components/bookComponents/AddForm.js
+++ src/components/bookComponents/AddForm.js
@@ -1,34 +1,49 @@
-import React, { useState} from 'react';
-import { useDispatch } from 'react-redux'
-import { addBook } from '../../redux/books/books';
-import { v4 as uuidv4 } from 'uuid';
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { addBook } from "../../redux/books/books";
+import { v4 as uuidv4 } from "uuid";
 
-const dispatch = useDispatch();
+const AddForm = () => {
+  const dispatch = useDispatch();
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
 
-const addBook = (e) => {
-  e.preventDefault();
-  if(title === "" || author === ""){
-    return;
-  } 
-  
-  const newBook ={
-    id: uuidv4(),
-    title,
-    author
+  const addBookToStore = (e) => {
+    e.preventDefault();
+    if (title === '' || author === '') return;
+    const newBook = {
+      id: uuidv4(),
+      title,
+      author,
+    };
+    dispatch(addBook(newBook));
+    setTitle('');
+    setAuthor('');
   }
-  dispatch(addBook(newBook));
-  setTitle('');
-  setAuthor('');
-}
-const AddForm = () => (
+  return (
   <form className="form">
-    <input required className="input-title" type="text" placeholder="Book title..." value={title}
-    onChange={(e) => setTitle(e.target.value)}/>
-    <input required className="input-author" type="text" placeHolder="Book author..." value={author}
-    onChange={(e) => setAuthor(e.target.value)}/>
+    <input
+      required
+      className="input-title"
+      type="text"
+      placeholder="Book title..."
+      value={title}
+      onChange={(e) => setTitle(e.target.value)}
+    />
+    <input
+      required
+      className="input-author"
+      type="text"
+      placeHolder="Book author..."
+      value={author}
+      onChange={(e) => setAuthor(e.target.value)}
+    />
     <input className="input-category" type="text" placeholder="Category" />
-    <button className="add-book-btn" type="submit" onClick={addBook}>Add Book</button>
+    <button className="add-book-btn" type="submit" onClick={addBook}>
+      Add Book
+    </button>
   </form>
 );
+}
 
 export default AddForm;
