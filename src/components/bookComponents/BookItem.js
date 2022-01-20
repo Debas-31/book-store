8e5c1b3d9dc8dde07a22de0cb71c87dbe5c23559
Debas Gebreslasie <65129228+Debas-31@users.noreply.github.com>
2022-01-20 13:59:15 +0300

    Implement a remove-book action in book-item

--- src/components/bookComponents/BookItem.js
+++ src/components/bookComponents/BookItem.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { useDispatch } from 'react-redux';
-import { removeBook } from '../../redux/books/books';
+import { removeBookApi } from '../../redux/books/books';
 
 const BookItem = (props) => {
   const {
@@ -10,7 +10,7 @@ const BookItem = (props) => {
 
   const dispatch = useDispatch();
   const removeBookFromStore = () => {
-    dispatch(removeBook({ id }));
+    dispatch(removeBookApi({ id }));
   };
 
   return (
